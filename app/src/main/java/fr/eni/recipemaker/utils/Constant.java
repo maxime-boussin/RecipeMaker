95e9eadaf67364cedf4e895c60ec636be46150f3
maxime-boussin <maxime.boussin35@gmail.com>
2020-04-17 17:02:14 +0200

    Fixes

---
+++ app/src/main/java/fr/eni/recipemaker/utils/Constant.java
@@ -0,0 +1,12 @@
+package fr.eni.recipemaker.utils;
+
+public class Constant {
+    public static final String id_Edamam = "395b9d8b";
+    public static final String key_Edamam = "43d56e1aaaec7f72dfecc245af8d536f";
+    public static final String get_recipe = "https://api.edamam.com/search?q=%s&app_id=%s&app_key=%s";
+    public static final String STR_EXCLUDED = "&healthLabels=";
+    public static final String ALCOHOL_FREE = "alcohol-free";
+    public static final String DAIRY_FREE = "dairy-free";
+    public static final String PEANUTS_FREE = "peanut-free";
+    public static final String GLUTEN_FREE = "gluten-free";
+}
